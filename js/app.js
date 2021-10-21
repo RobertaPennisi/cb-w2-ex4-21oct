a21af6cad156cab06cb0d9a0ff82aa2ae86046e2
Roberta Pennisi <roberta.pennisi97@gmail.com>
2021-10-21 11:04:12 +0200

    prime prove ciclo for

---
+++ js/app.js
@@ -0,0 +1,8 @@
+// Cicli Javascript
+
+let nomi = ['Giorgio', 'Francesca', 256, false, 'Pippo', 'Pluto'];
+
+for (let index = 0; index < nomi.length; index++)
+{
+    console.log((index + 1) + ":" + nomi[index]);
+}
