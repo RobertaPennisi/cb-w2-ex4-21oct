3d6ce181d8071260a309fbcd885e3da6c25455f7
Roberta Pennisi <roberta.pennisi97@gmail.com>
2021-10-21 19:14:06 +0200

    esercizio svolto

---
+++ js/cart.js
@@ -0,0 +1,27 @@
+const cart = [];
+
+
+
+function carrello(){
+    
+    let input = prompt('Cosa metti nel carrello?');
+
+    if(input===null || input==="" || parseInt(input)){
+        alert("inserisci qualcosa nel carrello");
+    } 
+    if(cart.includes(input) == true) {
+        alert("hai già aggiunto questo prodotto:" + " " + input)
+    }
+    
+    else if( input!=='') {
+        alert("Ben fatto! Hai aggiunto:" + " " + input)
+        cart.push(input)
+        console.log(cart)
+        
+        return input
+    }
+   
+    
+    
+}
+
