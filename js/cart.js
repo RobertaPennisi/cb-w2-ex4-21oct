1b34152d79235f6f75944b45e4b5ffc6a66b8e75
Roberta Pennisi <roberta.pennisi97@gmail.com>
2021-10-21 19:22:29 +0200

    pulizia files

--- js/cart.js
+++ js/cart.js
@@ -7,7 +7,7 @@ function carrello(){
     let input = prompt('Cosa metti nel carrello?');
 
     if(input===null || input==="" || parseInt(input)){
-        alert("inserisci qualcosa nel carrello");
+        alert("Non hai aggiunto alcun prodotto");
     } 
     if(cart.includes(input) == true) {
         alert("hai già aggiunto questo prodotto:" + " " + input)
